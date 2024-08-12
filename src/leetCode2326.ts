c7b6e0661ae26fb3837fe718a1626c73a2da846b
fauzil27 <fauziladhim706@gmail.com>
2024-08-12 18:52:12 +0800

    leetcode2326

---
+++ src/leetCode2326.ts
@@ -0,0 +1,71 @@
+// let m = 3,
+//   n = 5;
+let m = 1,
+  n = 4,
+  head = [0, 1, 2];
+
+interface ListNode {
+  val: number;
+  next: ListNode | null;
+}
+
+// let head = [3, 0, 2, 6, 8, 1, 7, 9, 4, 2, 5, 5, 0];
+
+function coverter(arr: number[]) {
+  var list = null;
+  for (let i = arr.length - 1; i >= 0; i--) {
+    list = { val: arr[i], next: list };
+  }
+  console.log(list);
+  return list;
+}
+
+// console.log(coverter(head));
+function spiralMatrix(m: number, n: number, head: ListNode | null) {
+  let result: number[][] = [];
+  let num = 0;
+  let kiri = 0;
+  let kanan = n - 1;
+  let atas = 0;
+  let bawah = m - 1;
+
+  for (let i = 0; i < m; i++) {
+    let tmp = [];
+    for (let k = 0; k < n; k++) {
+      tmp.push(-1);
+    }
+    result.push(tmp);
+  }
+
+  while (atas <= bawah && kiri <= kanan && head) {
+    for (let i = kiri; i <= kanan && head; i++) {
+      result[atas][i] = head.val;
+      num++;
+      head = head?.next;
+    }
+    atas++;
+
+    for (let i = atas; i <= bawah && head; i++) {
+      result[i][kanan] = head?.val;
+      head = head?.next;
+    }
+    kanan--;
+    if (atas <= bawah) {
+      for (let i = kanan; i >= kiri && head; i--) {
+        result[bawah][i] = head?.val;
+        head = head?.next;
+      }
+      bawah--;
+    }
+    if (kiri <= kanan) {
+      for (let i = bawah; i >= atas && head; i--) {
+        result[i][kiri] = head.val;
+        head = head.next;
+      }
+      kiri++;
+    }
+  }
+  return result;
+}
+
+console.log(spiralMatrix(m, n, coverter(head)));
