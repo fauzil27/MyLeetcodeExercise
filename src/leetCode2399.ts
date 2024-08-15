4f050f4a36610bf6e33bffecc41a0b768383f0ef
fauzil27 <fauziladhim706@gmail.com>
2024-08-15 11:05:58 +0800

    leetCode2399

--- src/leetCode2399.ts
+++ src/leetCode2399.ts
@@ -1,8 +1,8 @@
-let st = "abaccb",
-  distance = [
-    1, 3, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0,
-  ];
+// let st = "abaccb",
+//   distance = [
+//     1, 3, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//     0,
+//   ];
 
 // let st = "aa",
 //   distance = [
@@ -16,38 +16,24 @@ let st = "abaccb",
 //     0,
 //   ];
 
-// let st = "abbccddeeffgghhiijjkkllmmnnooppqqrrssttuuvvwwxxyyzza",
-//   distance = [
-//     50, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-//     0,
-//   ];
+let st = "abbccddeeffgghhiijjkkllmmnnooppqqrrssttuuvvwwxxyyzza",
+  distance = [
+    50, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0,
+  ];
 
 function checkDistances(s: string, distance: number[]) {
-  function remove(s: string) {
-    let result1 = "";
-    for (let i = 0; i < s.length; i++) {
-      if (!result1.includes(s[i])) {
-        result1 += s[i];
-      }
-    }
-    return result1;
-  }
-  let s2 = remove(s);
   let result = true;
   let latter: string = "abcdefghijklmnopqrstuvwxyz";
-  //   console.log(s2);
   for (let i = 0; i < distance.length; i++) {
-    // console.log(
-    //   s2[i],
-    //   s[s.indexOf(s2[i]) + distance[latter.indexOf(s2[i])] + 1],
-    //   distance[latter.indexOf(s2[i])],
-    //   s.indexOf(s2[i])
-    // );
-    if (s2.includes(s[i])) {
-      if (s2[i] !== s[s.indexOf(s2[i]) + distance[latter.indexOf(s2[i])] + 1]) {
+    if (s.includes(latter[i])) {
+      if (
+        latter[i] !==
+        s[s.indexOf(latter[i]) + distance[latter.indexOf(latter[i])] + 1]
+      ) {
         result = false;
       }
-      if (s2[i] === undefined) {
+      if (latter[i] === undefined) {
         break;
       }
     }
